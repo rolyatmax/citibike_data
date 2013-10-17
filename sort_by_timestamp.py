f3d7ac139ddc6b1f03e6522e2564f8a845e1d80d
Baldwin <205421@rndbaldt-mbair.nyhq.nytint.com>
2013-10-17 11:07:52 -0400

    forgot to save this stuff

--- sort_by_timestamp.py
+++ sort_by_timestamp.py
@@ -2,14 +2,18 @@
 import json
 from pymongo import MongoClient
 from bson.json_util import dumps
+from underscore import _
 
 client = MongoClient()
 
 db = client.citibike
 data_file = open('data/sorted_data.json', 'w+')
 
-data = []
+### this is returning a <pymongo.cursor.Cursor object at 0x10702ab50>
+snapshots = db.bikes.find()
 
+#### maybe that's why this line isn't working....
+data = _.groupBy(snapshots, lambda snapshot, *a: snapshot.executionTime)
 
 out = data
 json.dump(out, data_file, indent=True)
