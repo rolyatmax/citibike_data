335a7060e72ddd749511969e67fc00d3132a5c30
Taylor Baldwin <taylorbaldwin@gmail.com>
2013-10-16 23:35:25 -0400

    trying to figure out aggregation

--- sort_by_timestamp.py
+++ sort_by_timestamp.py
@@ -8,10 +8,15 @@ client = MongoClient()
 db = client.citibike
 data_file = open('data/sorted_data.json', 'w+')
 
+cursor = db.bikes.find()
+
 data = []
 
+for i in cursor:
+	data.append(i)
 
-out = data
-json.dump(out, data_file, indent=True)
+
+#out = data
+#json.dump(out, data_file, indent=True)
 
 data_file.close()
