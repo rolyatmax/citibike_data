8acfeb782922c3425207c36a7d42690186c53a80
Taylor Baldwin <taylorbaldwin@gmail.com>
2013-10-18 01:05:45 -0400

    trying to map coordinates with canvas coords - having problems

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-CitiBike Data Visualization
+CitiBike: An Average Summer Week
 ===========================
 
-Still working on this project for now.
-
-More info to come here soon.
+Nearly 1GB of data was collected from the CitiBike API during the summer of 2013 by my colleague Jason Capehart. I crunched the numbers to see what the typical summer week was like for the CitiBike stations.
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // The Main JS File
 
-var svg;
+var ctx;
 var canvas;
 
 (function(){
@@ -8,30 +8,63 @@ var canvas;
 	var width = 960;
 	var height = 1150;
 
+
+	/// with this projection it appears that the coords
+	/// relate to the canvas in the following way:
+	/// AGH! BUT IT'S NOT WORKING! HELP!
+	///
+	/// Latitude
+	/// 40.768054 : 73
+	/// 40.714838 : 833
+	/// 40.708080 : 991
+	///
+	/// Longitude
+	/// -73.981918 : 384
+	/// -73.976441 : 626
+	/// -73.999401 : 398
+
+
+
+
 	var proj = d3.geo.albers()
-		// .center([40, -74])
+		// .center([-74, 40])
 		// .rotate([0, 0])
 		// .parallels([50, 60])
 		.scale( 850000 )
 		.translate([-249600, 59300]);
 
-	canvas = d3.select('#container').append('canvas')
+	ctx = Sketch.create({
+		container: document.getElementById('container'),
+		autostart: false,
+		fullscreen: false,
+		autoclear: false
+	});
+
+	canvas = d3.select('canvas')
 		.attr('width', width)
 		.attr('height', height);
 
-	var ctx = canvas.node().getContext('2d');
-
-	var path = d3.geo.path()
-		.projection( proj )
-		.context( ctx );
-
 	d3.json('shapefiles/manhattan_roads.json', function(err, manhattan) {
-		console.log(manhattan);
 
+		canvas.attr('style', 'opacity:1');
+
+		var path = d3.geo.path()
+			.projection( proj )
+			.context( ctx );
 
 		path( topojson.feature(manhattan, manhattan.objects.manhattan_roads) );
 		ctx.strokeStyle = 'rgba(0,0,0,0.4)';
 		ctx.stroke();
+
+		ctx.click = function() {
+			console.log('mouse', ctx.mouse.x, ctx.mouse.y);
+		};
 	});
+
+
+
+
+
+
 
 })();
